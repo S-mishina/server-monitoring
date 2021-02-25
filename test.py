a4099311ba2df186f9bb01255700bdb52eb56a0b
MISHINA <183244r4@stu.teikyo-u.ac.jp>
2021-02-25 23:19:33 +0900

    １分ごとにメモリの使用料を可視化する

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-import psutil 
+import time
+import psutil
+import datetime
 
-# メモリ容量を取得
-mem = psutil.virtual_memory() 
-print(mem.used/mem.total)
+while True:
+    # メモリ容量を取得
+    time.sleep(60)
+    dt_now = datetime.datetime.now()
+    print(dt_now.strftime('%H:%M'))
+    mem = psutil.virtual_memory() 
+    print((mem.used/mem.total)*100)
