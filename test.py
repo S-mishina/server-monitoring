7a90f86fb0cd7cd5f6c6217dc2ea116c4943f46c
MISHINA <183244r4@stu.teikyo-u.ac.jp>
2021-02-26 00:14:01 +0900

    メモリ,cpuの使用量をsqlに送信する為のプログラム作成

--- test.py
+++ test.py
@@ -1,11 +1,38 @@
+''' server-monitoring'''
 import time
-import psutil
 import datetime
-
+import psutil
+import pymysql
+def instartdb(time1,mem_used,mem_total,disk_used,disk_total):
+    '''
+    instartdb はサーバの状態を取得してdbに出力する為の機能
+    '''
+    con = pymysql.connect(
+            host='127.0.0.1',
+            user='root',
+            password='root',
+            db='seerver_m',
+            port= 13306,
+            charset='utf8',
+            cursorclass=pymysql.cursors.DictCursor,
+        )
+    con.ping(reconnect=True)
+    cur = con.cursor()
+    query="INSERT INTO monitoring (time,mem_used,mem_total,dsk_used,dsk_total) \
+        VALUES (%s,%s,%s,%s,%s) ;"
+    cur.execute(query, (time1,mem_used,mem_total,dsk_used,dsk_total))
+    con.commit()
+    cur.close()
+    con.close()
 while True:
-    # メモリ容量を取得
     time.sleep(60)
     dt_now = datetime.datetime.now()
-    print(dt_now.strftime('%H:%M'))
-    mem = psutil.virtual_memory() 
-    print((mem.used/mem.total)*100)
+    time1=dt_now.strftime('%H:%M')
+    mem = psutil.virtual_memory()
+    mem_used=mem.used
+    mem_total=mem.total
+    dsk = psutil.disk_usage('/')
+    dsk_used=dsk.used
+    dsk_total=dsk.total
+    instartdb(time1,mem_used,mem_total,dsk_used,dsk_total)
+    
